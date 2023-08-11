dda24aa2f11b54d4b107fdd00b2ecf2f68d7e2c3
Jussi Saarinen <jussi.saarinen_c@sandvik.com>
2023-08-11 10:23:24 +0300

    Section 4 part 34. Thin Controllers in the API

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,17 +9,11 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
 
         [HttpGet] //api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
         [HttpGet("{id}")] //api/activities/fdfff
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -6,6 +7,12 @@ namespace API.Controllers
     [Route("api/[controller]")] // localhost:5000/weatherforecast (class name minus Controller)
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
 
+        // protected property Mediator can be used in any derived class
+        // if _mediator has not been initialized, the property will initialize it
+        // calling HttpContext.RequestServices.GetService<IMediator>();
+        protected IMediator Mediator => _mediator ??= 
+            HttpContext.RequestServices.GetService<IMediator>();
     }
 }
