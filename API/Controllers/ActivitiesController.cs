3a1dbb51d2cd4907487298495846b0b1c0d28bb8
Jussi Saarinen <jussi.saarinen_c@sandvik.com>
2023-08-14 11:29:59 +0300

    Section 4: part 41. Cancellation token demo.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,9 +11,9 @@ namespace API.Controllers
     {
 
         [HttpGet] //api/activities
-        public async Task<ActionResult<List<Activity>>> GetActivities()
+        public async Task<ActionResult<List<Activity>>> GetActivities(CancellationToken ct)
         {
-            return await Mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query(), ct);
         }
 
         [HttpGet("{id}")] //api/activities/fdfff
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -13,13 +13,28 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private ILogger<List> _logger;
+            public Handler(DataContext context, ILogger<List> logger)
             {
+                _logger = logger;
                 _context = context;
             }
             // cancellation token allow cancel a request when it's no longer needed
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
+                try
+                {
+                    for(var i = 0; i < 10; i++)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        await Task.Delay(1000, cancellationToken);
+                        _logger.LogInformation($"Task {i} has completed");
+                    }
+                }
+                catch(System.Exception)
+                {
+                    _logger.LogInformation("Task was cancelled");
+                }
                 return await _context.Activities.ToListAsync();
             }
         }
