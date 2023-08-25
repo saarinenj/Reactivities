ecd36d651acafd7a5f89fad6cdb2f256ab3d333d
Jussi Saarinen <jussi.saarinen_c@sandvik.com>
2023-08-25 11:26:05 +0300

    Section 8-83: Getting an individual activity.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,6 @@
-import { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
 import { observer } from 'mobx-react-lite';
-import { useStore } from '../stores/store';
 import { Outlet } from 'react-router-dom';
 
 function App() {
