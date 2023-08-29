3a886fd0287ef5cd02cc12a2210b9c08802297b1
Jussi Saarinen <jussi.saarinen_c@sandvik.com>
2023-08-29 10:43:31 +0300

    End of Section 9.

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,8 +1,8 @@
-import { Header, Item, Segment } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import ActivityListItem from './ActivityListItem';
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 
 export default observer(function ActivityList() {
 
