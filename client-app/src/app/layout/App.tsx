2121a36e6dda751f8d32d60c8b5525d8c45a8103
Jussi Saarinen <jussi.saarinen_c@sandvik.com>
2023-10-17 11:23:53 +0300

    109: Using an interceptor to handle API error responses.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
 import { Outlet, useLocation } from 'react-router-dom';
 import HomePage from '../../home/HomePage';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const location = useLocation();
