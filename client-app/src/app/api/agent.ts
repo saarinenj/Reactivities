271b8d98a22deb05c3458afe679e387d7d993127
Jussi Saarinen <jussi.saarinen_c@sandvik.com>
2023-10-17 16:41:19 +0300

    End of section 10.

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -17,9 +17,14 @@ axios.interceptors.response.use(async response => {
         await sleep(1000);
         return response;
 }, (error: AxiosError) => {
-    const {data, status} = error.response as AxiosResponse;
+    const {data, status, config} = error.response as AxiosResponse;
     switch (status){
         case 400:
+            // alternative way to achieve same if using Vite to avoid linting warning:
+            // if(config.method == 'get' && Object.prototype.hasOwnProperty.call(data.errors, 'id')){}
+            if(config.method == 'get' && data.errors.hasOwnProperty('id')){
+                router.navigate('/not-found');
+            }
             if(data.errors){
                 const modalStateErrors = [];
                 for(const key in data.errors){
