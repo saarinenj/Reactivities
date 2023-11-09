e9a63a6a9f8c58e171cb60ec82b7a2e721d0e366
Jussi Saarinen <jussi.saarinen_c@sandvik.com>
2023-11-09 12:35:49 +0200

    122. Creating a reusable date input

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,12 +2,15 @@ import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
 import { StoreContext, store } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
+import {createBrowserHistory} from 'history';
 
+export const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
