ca78359bf0e28b6bdb129fbb6e27e849d9bb4803
Jussi Saarinen <jussi.saarinen_c@sandvik.com>
2023-12-11 15:01:45 +0200

    152. Adding Modals, added missing "Login to Reactivities" text to login form.

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { ErrorMessage, Form, Formik } from "formik";
-import { Button, Label } from "semantic-ui-react";
+import { Button, Header, Label } from "semantic-ui-react";
 import MyTextInput from "../../app/common/form/MyTextInput";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../app/stores/store";
@@ -14,6 +14,7 @@ export default observer(function LoginForm() {
         >
             {({handleSubmit, isSubmitting, errors}) => (
                 <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                    <Header as='h2' content="Login to Reactivities" color="teal" textAlign="center" />
                     <MyTextInput placeholder="email" name='email' />
                     <MyTextInput placeholder="password" name='password' type='password' />
                     <ErrorMessage
