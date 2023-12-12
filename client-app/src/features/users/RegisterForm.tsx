143cfd06ab93e8fe5f603e651999884ad43bce3f
Jussi Saarinen <jussi.saarinen_c@sandvik.com>
2023-12-12 12:25:19 +0200

    End of Section 13: 154. Handling validation errors in the register form.

--- client-app/src/features/users/RegisterForm.tsx
+++ client-app/src/features/users/RegisterForm.tsx
@@ -4,6 +4,7 @@ import MyTextInput from "../../app/common/form/MyTextInput";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../app/stores/store";
 import * as Yup from 'yup';
+import ValidationError from "../activities/errors/ValidationError";
 
 export default observer(function RegisterForm() {
     const { userStore } = useStore();
@@ -11,7 +12,8 @@ export default observer(function RegisterForm() {
         <Formik
             initialValues={{ displayName: '', username: '', email: '', password: '', error: null }}
             onSubmit={(values, { setErrors }) =>
-                userStore.register(values).catch(error => setErrors({ error: 'Invalid email or password' }))}
+                userStore.register(values).catch(error => 
+                    setErrors({ error }))}
             validationSchema={Yup.object({
                 displayName: Yup.string().required(),
                 username: Yup.string().required(),
@@ -21,7 +23,7 @@ export default observer(function RegisterForm() {
             })}
             >
             {({ handleSubmit, isSubmitting, errors, isValid,  dirty }) => (
-                <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                <Form className='ui form error' onSubmit={handleSubmit} autoComplete='off'>
                     <Header as='h2' content="Sign up to Reactivities" color="teal" textAlign="center" />
                     <MyTextInput placeholder="Display Name" name='displayName' />
                     <MyTextInput placeholder="Username" name='username' />
@@ -29,8 +31,7 @@ export default observer(function RegisterForm() {
                     <MyTextInput placeholder="password" name='password' type='password' />
                     <ErrorMessage
                         name='error' render={() =>
-                            <Label style={{ marginBottom: 10 }} basic color='red' content={errors.error} />}
-                    />
+                            <ValidationError errors={errors.error as unknown as string[]}/>}/>
                     <Button
                         disabled={!isValid || !dirty || isSubmitting}
                         loading={isSubmitting} 
