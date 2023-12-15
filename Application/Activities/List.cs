46ed92ddc7915c548d7588205ce8ae8d4dfd950b
Jussi Saarinen <jussi.saarinen_c@sandvik.com>
2023-12-15 14:05:29 +0200

    162: Shaping the related data

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -4,29 +4,36 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Persistence;
 using Application.Core;
+using AutoMapper;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> {}
+        public class Query : IRequest<Result<List<ActivityDto>>> {}
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext _context;
 
-            public Handler(DataContext context)
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
             // cancellation token allow cancel a request when it's no longer needed
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
                     .Include(a => a.Attendees)
                     .ThenInclude(u => u.AppUser)
                     .ToListAsync(cancellationToken);
-                return Result<List<Activity>>.Success(activities);
+
+                var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
+
+                return Result<List<ActivityDto>>.Success(activitiesToReturn);
             }
         }
     }
