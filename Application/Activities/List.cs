1983989e0d5396d0b1502f25361b08488f5e3ae6
Jussi Saarinen <jussi.saarinen_c@sandvik.com>
2023-12-15 07:59:02 +0200

    161: Loading related data

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -22,7 +22,11 @@ namespace Application.Activities
             // cancellation token allow cancel a request when it's no longer needed
             public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync(cancellationToken));
+                var activities = await _context.Activities
+                    .Include(a => a.Attendees)
+                    .ThenInclude(u => u.AppUser)
+                    .ToListAsync(cancellationToken);
+                return Result<List<Activity>>.Success(activities);
             }
         }
     }
